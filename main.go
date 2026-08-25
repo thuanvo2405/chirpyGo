0e656221a2bd5ad1165d641a34e209836bb295c0
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-25 21:44:24 +0700

    Custom Handlers

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	s := &http.Server{
 		Addr:    ":8080",
