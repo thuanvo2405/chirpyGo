055b47c990af807e555359f74a91831940bc0454
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-26 11:56:22 +0700

    JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -37,6 +38,28 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hits reset to 0"))
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, returnVals{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	apiCfg := apiConfig{
@@ -51,6 +74,31 @@ func main() {
 	})
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Something went wrong")
+			return
+		}
+
+		const maxChirpLength = 140
+		if len(params.Body) > maxChirpLength {
+			respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+			return
+		}
+
+		respondWithJSON(w, 200, struct {
+			Valid bool `json:"valid"`
+		}{
+			Valid: true,
+		})
+	})
 
 	s := &http.Server{
 		Addr:    ":8080",
