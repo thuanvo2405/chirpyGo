c4619d01addeb5f4cd7b30c94e6f993318d5c786
thuanvo2405 <vothuan2405003@gmail.com>
2026-08-27 14:14:14 +0700

    Authorization

--- main.go
+++ main.go
@@ -392,6 +392,57 @@ func main() {
 		respondWithJSON(w, http.StatusOK, chirp)
 	})
 
+	mux.HandleFunc("PUT /api/users", func(w http.ResponseWriter, r *http.Request) {
+		token, err := auth.GetBearerToken(r.Header)
+		if err != nil {
+			respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT")
+			return
+		}
+
+		userID, err := auth.ValidateJWT(token, apiCfg.jwtSecretKey)
+		if err != nil {
+			respondWithError(w, http.StatusUnauthorized, "Invalid token")
+			return
+		}
+
+		type paramaters struct {
+			Password string `json:"password"`
+			Email    string `json:"email"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := paramaters{}
+		err = decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid request body")
+			return
+		}
+
+		hashedPassword, err := auth.HashPassword(params.Password)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't hash password")
+			return
+		}
+
+		updatedUser, err := apiCfg.database.UpdateUser(r.Context(), database.UpdateUserParams{
+			Email:          params.Email,
+			HashedPassword: hashedPassword,
+			UpdatedAt:      time.Now().UTC(),
+			ID:             userID,
+		})
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't update user")
+			return
+		}
+
+		respondWithJSON(w, http.StatusOK, User{
+			ID:        updatedUser.ID,
+			CreatedAt: updatedUser.CreatedAt,
+			UpdatedAt: updatedUser.UpdatedAt,
+			Email:     updatedUser.Email,
+		})
+	})
+
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -16,3 +16,9 @@ DELETE FROM users;
 -- name: GetUserByEmail :one
 SELECT * FROM users
 WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET email = $1, hashed_password = $2, updated_at = $3
+WHERE id = $4
+RETURNING *;
